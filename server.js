e7103d5cfb58042b40e9a07d6a5f49494432cb0a
Shrimp <moumoon711@gmail.com>
2016-06-05 10:29:37 +0800

    test

--- server.js
+++ server.js
@@ -32,32 +32,32 @@ app.get('/partials/:partialPath', function (req, res) {
 
 app.get('*', function (req, res) {
     res.render('index', {
-     //   mongoMessage: mongoMessage
+        mongoMessage: mongoMessage
     })
 })
 
-app.listen(3030)
+app.listen(process.env.PORT || 5000)
 console.log('server running localhost:3030');
 
 
-//if (env == "development") {
-//    mongoose.connect('mongodb://localhost/mean');
-//} else {
-//    mongoose.connect('mongodb://tang:tangzx711@ds015403.mlab.com:15403/mean');
-//}
-//
-//var db = mongoose.connection;
-//db.on('error', function () {
-//    console.error('connection error...')
-//});
-//db.once('open', function () {
-//    console.log('db connection')
-//});
-//
-//var mongoMessage;
-//var Message = mongoose.model('Message', mongoose.Schema({message: String}));
-//
-//Message.findOne({}).exec(function (err, doc) {
-//    console.log(doc)
-//    mongoMessage = doc.message;
-//})
+if (env == "development") {
+    mongoose.connect('mongodb://localhost/mean');
+} else {
+    mongoose.connect('mongodb://tang:tangzx711@ds015403.mlab.com:15403/mean');
+}
+
+var db = mongoose.connection;
+db.on('error', function () {
+    console.error('connection error...')
+});
+db.once('open', function () {
+    console.log('db connection')
+});
+
+var mongoMessage;
+var Message = mongoose.model('Message', mongoose.Schema({message: String}));
+
+Message.findOne({}).exec(function (err, doc) {
+    console.log(doc)
+    mongoMessage = doc.message;
+})
